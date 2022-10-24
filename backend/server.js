448e7afb924bd5809a5144c7ad779b2445aa96ad
jess <jessica.response201@gmail.com>
2022-10-24 20:19:35 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,14 @@ const fileUpload = require("express-fileupload");
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: 'https://foodnary.netlify.app/',
+  optionsSuccessStatus: 200 // For legacy browser support
+}
+
+
+app.use(cors(corsOptions));
 app.use(
   fileUpload({
     useTempFiles: true
